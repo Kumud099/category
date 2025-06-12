021ae0b8bc37cbd9ce2b060b45d523474e071ef8
Kumud shrestha <shresthakumud5@gmail.com>
2025-06-12 16:18:44 +0545

    Set up environment and configuration for Django project with Docker support

--- core/settings.py
+++ core/settings.py
@@ -11,16 +11,21 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+load_dotenv(dotenv_path =BASE_DIR / '.env')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "your-default-secret-key")
+SECRET_KEY = SECRET_KEY = os.environ.get("SECRET_KEY")
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -28,7 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
 
-# Application definition
+# Application definitionas
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -48,7 +53,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = "core.urls"
 
@@ -79,8 +87,8 @@ DATABASES = {
         'NAME': os.environ.get("DB_NAME"),             
         'USER': os.environ.get("DB_USER"),    
         'PASSWORD': os.environ.get("DB_PASSWORD"),          
-        'HOST': '127.0.0.1',            
-        'PORT': '5433',
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
@@ -127,7 +135,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
